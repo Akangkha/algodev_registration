a570b947ed147364438caaa70e73457f80b965f1
KingsukNandi <kingsuk24nandi@gmail.com>
2024-09-01 16:55:22 +0530

    Add animations Form submission currently not working

---
+++ src/components/PokeballInput.tsx
@@ -0,0 +1,67 @@
+import { FieldErrors, UseFormRegister } from "react-hook-form";
+import pokeball from "../assets/pokeball.svg";
+import { formData } from "./Registration";
+
+interface PokeballInputProps {
+  name: keyof formData;
+  label: string;
+  register: UseFormRegister<formData>;
+  errors: FieldErrors<formData>;
+  focusedField: string;
+  handleFocus: (event: React.FocusEvent<HTMLInputElement>) => void;
+  handleBlur: () => void;
+  value?: string;
+  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  disabled?: boolean;
+}
+const PokeballInput: React.FC<PokeballInputProps> = ({
+  name,
+  label,
+  register,
+  errors,
+  focusedField,
+  handleFocus,
+  handleBlur,
+  value,
+  onChange,
+  disabled,
+}) => {
+  console.log(register);
+  return (
+    <div className="flex flex-col relative">
+      <label
+        htmlFor={name}
+        className="text-white xl:text-xl text-base font-bold"
+      >
+        {label}
+      </label>
+      <div className="flex items-center">
+        <img
+          src={pokeball}
+          alt=""
+          className={`md:size-12 xl:size-16 size-11 absolute md:-left-7 -left-5 transition-all ${
+            focusedField === name ? "rotate-[-26deg]" : ""
+          }`}
+        />
+        <input
+          type="text"
+          id={name}
+          {...register(name)}
+          value={value}
+          onChange={onChange}
+          disabled={disabled}
+          className="bg-white xl:pl-8 pl-6 xl:p-4 p-2 rounded-xl text-lg w-full"
+          onFocus={(e) => handleFocus(e)}
+          onBlur={handleBlur}
+        />
+      </div>
+      {errors[name] && (
+        <span className="text-yellow mx-2 px-2 text-sm font-bold">
+          * {errors[name].message}
+        </span>
+      )}
+    </div>
+  );
+};
+
+export default PokeballInput;
--- src/components/Registration.tsx
+++ src/components/Registration.tsx
@@ -5,72 +5,96 @@ import ash from "../assets/ash.svg";
 // import pikachu from "../assets/pikachu.svg";
 import snorlax from "../assets/snorlax.svg";
 import pokeball from "../assets/pokeball.svg";
+import { useState } from "react";
+import PokeballInput from "./PokeballInput";
 
-interface formData {
+export interface formData {
   firstName: string;
   lastName: string;
   phone: string;
   email: string;
   whatsapp: string;
-  isSame: boolean;
+  isSame?: string;
 }
 const schema: ZodType<formData> = z.object({
-  firstName: z.string({ message: "This field is required" }),
-  // .max(30, { message: "First name must be less than 30 characters" })
-  // .min(1, { message: "First name must be more than 1 characters" }),
-  lastName: z.string({ message: "This field is required" }),
-  // .max(30, { message: "Last name must be less than 30 characters" })
-  // .min(1, { message: "Last name must be more than 1 characters" }),
-  email: z.string({ message: "This field is required" }),
-  // .email({ message: "Enter valid email" }),
-  phone: z.string({ message: "This field is required" }),
-  // .regex(
-  // /^\s*(?:\+?(\d{1,3}))?([-. (]*(\d{3})[-. )]*)?((\d{3})[-. ]*(\d{2,4})(?:[-.x ]*(\d+))?)\s*$/gm,
-  // { message: "Enter valid phone number" }
-  // ),
+  firstName: z
+    .string({ message: "First name is required" })
+    .max(30, { message: "First name must be less than 30 characters" })
+    .min(1, { message: "First name is required" }),
+  lastName: z
+    .string({ message: "Last name is required" })
+    .max(30, { message: "Last name must be less than 30 characters" })
+    .min(1, { message: "Last name is required" }),
+  email: z
+    .string({ message: "Email is required" })
+    .email({ message: "Enter valid email" }),
+  phone: z
+    .string({ message: "Phone number is required" })
+    .regex(
+      /^\s*(?:\+?(\d{1,3}))?([-. (]*(\d{3})[-. )]*)?((\d{3})[-. ]*(\d{2,4})(?:[-.x ]*(\d+))?)\s*$/gm,
+      { message: "Enter valid phone number" }
+    ),
   whatsapp: z.string(),
-  isSame: z.boolean(),
+  isSame: z.string().optional(),
 });
 
 const Registration: React.FC = () => {
+  const [phoneValue, setPhoneValue] = useState("");
+  const [focusedField, setFocusedField] = useState("");
+
+  const handleFocus = (e: React.FocusEvent<HTMLInputElement>) => {
+    setFocusedField(e.target.id);
+  };
+
+  const handleBlur = () => {
+    setFocusedField("");
+  };
   const {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm<formData>({ resolver: zodResolver(schema) });
+    watch,
+    setValue,
+  } = useForm<formData>({ resolver: zodResolver(schema), mode: "onChange" });
 
   const submitForm = async (data: formData) => {
-    console.log(data);
+    try {
+      console.log("Form data:", data);
 
-    const formData = new FormData();
-    const params = new URLSearchParams();
-    for (const [key, value] of Object.entries(formData)) {
-      params.append(key, value);
-    }
+      const params = new URLSearchParams();
+      params.append("entry.521128088", String(data.firstName));
+      params.append("entry.1938341443", String(data.lastName));
+      params.append("entry.2027588860", String(data.email));
+      params.append("entry.1956564773", String(data.phone));
+      params.append("entry.922164047", String(data.whatsapp));
+      // params.append("entry.1669604705", data.isSame ? "true" : "false");
 
-    fetch(
-      `https://docs.google.com/forms/d/e/1FAIpQLSfBRZh39FYS-tCgLu51UdyozovyDK9_hMxkjW8R5SncsdjjEQ/formResponse?${params}`,
-      {
+      const googleFormUrl = import.meta.env.VITE_GOOGLE_FORMS_ENDPOINT;
+
+      const response = await fetch(`${googleFormUrl}?${params.toString()}`, {
         method: "POST",
         mode: "no-cors",
-      } // this posts the data to google sheets linked with the google form but the values passed are empty
-    )
-      .then((response) => {
-        console.log(response);
-        if (response.ok) {
-          console.log("Form submitted successfully!");
-        } else {
-          console.error("Error submitting form1:", response.status);
-        }
-      })
-      .catch((error) => console.error("Error submitting form:", error));
+      });
+      console.log(params.toString());
+      if (response.ok) {
+        console.log("Form submitted successfully!");
+      } else {
+        console.error(
+          "Error submitting form:",
+          response.status,
+          response.statusText
+        );
+      }
+    } catch (error) {
+      console.error("Error submitting form:", error);
+    }
   };
   return (
     <div className="w-full h-screen flex relative">
       <img
         src={ash}
         alt=""
-        className="hidden xl:block size-[500px] absolute -translate-x-1/2 bottom-0 left-1/2  z-10"
+        className="hidden xl:block size-[500px] absolute -translate-x-1/2 bottom-0 left-1/2 z-10"
       />
       <div className="w-1/2 bg-yellow md:flex justify-center items-center relative hidden">
         <img
@@ -109,145 +133,74 @@ const Registration: React.FC = () => {
           xxx
         </div>
       </div>
-      <div className="xl:w-1/2 w-full bg-blue xl:p-12 p-4">
+      <div className="md:w-1/2 w-full bg-blue xl:p-12 p-4">
         <h1 className="text-white solid xl:text-7xl text-4xl m-auto w-fit xl:my-12 my-10 text-center">
           Register Now
         </h1>
         <div className="flex flex-col justify-center items-center h-max">
           <form
-            className="flex flex-col xl:gap-7 gap-2 montserrat z-20"
+            className="flex flex-col xl:px-7 md xl:gap-3 gap-2 montserrat z-20 w-4/5"
             onSubmit={handleSubmit(submitForm)}
           >
-            <div className="flex flex-col xl:flex-row xl:gap-14 gap-2 w-full">
-              <div className="flex flex-col xl:w-1/2 w-full relative">
-                <label
-                  htmlFor="firstName"
-                  className="text-white xl:text-xl text-base font-bold"
-                >
-                  First Name
-                </label>
-                <img
-                  src={pokeball}
-                  alt=""
-                  className="md:size-12 xl:size-16 size-11 absolute md:top-[22px] xl:top-[26px] top-[24px]  md:-left-[17px] xl:-left-[23px] -left-[15px]"
-                />
-                <input
-                  type="text"
-                  id="firstName"
-                  {...register("firstName")}
-                  name="entry.521128088"
-                  className="bg-white xl:p-4 xl:pl-[50px] p-2 rounded-xl text-lg md:pl-[50px] pl-[30px]"
-                />
-                {errors.firstName && (
-                  <span className="text-red-500 mx-2 px-2 text-sm">
-                    {errors.firstName.message}
-                  </span>
-                )}
-              </div>
+            <div className="flex flex-col xl:flex-row xl:gap-8 gap-2 w-full">
+              <PokeballInput
+                name="firstName"
+                label="First Name"
+                register={register}
+                errors={errors}
+                focusedField={focusedField}
+                handleFocus={handleFocus}
+                handleBlur={handleBlur}
+              />
 
-              <div className="flex flex-col xl:w-1/2 w-full relative">
-                <label
-                  htmlFor="lastName"
-                  className="text-white xl:text-xl text-base font-bold"
-                >
-                  Last Name
-                </label>
-                <img
-                  src={pokeball}
-                  alt=""
-                  className="md:size-12 xl:size-16 size-11 absolute md:top-[22px] xl:top-[26px] top-[24px]  md:-left-[17px] xl:-left-[23px] -left-[15px]"
-                />
-                <input
-                  type="text"
-                  id="lastName"
-                  {...register("lastName")}
-                  name="entry.1938341443"
-                  className="bg-white xl:p-4 xl:pl-[50px] p-2 rounded-xl text-lg md:pl-[50px] pl-[30px]"
-                />
-                {errors.lastName && (
-                  <span className="text-red-500 mx-2 px-2 text-sm">
-                    {errors.lastName.message}
-                  </span>
-                )}
-              </div>
-            </div>
-            <div className="flex flex-col relative">
-              <label
-                htmlFor="email"
-                className="text-white xl:text-xl text-base font-bold"
-              >
-                Email (Personal)
-              </label>
-              <img
-                src={pokeball}
-                alt=""
-                className="md:size-12 xl:size-16 size-11 absolute md:top-[22px] xl:top-[26px] top-[24px]  md:-left-[17px] xl:-left-[23px] -left-[15px]"
+              <PokeballInput
+                name="lastName"
+                label="Last Name"
+                register={register}
+                errors={errors}
+                focusedField={focusedField}
+                handleFocus={handleFocus}
+                handleBlur={handleBlur}
               />
-              <input
-                type="email"
-                id="email"
-                {...register("email")}
-                name="entry.2027588860"
-                className="bg-white xl:p-4 xl:pl-[50px] p-2 rounded-xl text-lg md:pl-[50px] pl-[30px]"
-              />
-              {errors.email && (
-                <span className="text-red-500 mx-2 px-2 text-sm">
-                  {errors.email.message}
-                </span>
-              )}
             </div>
 
-            <div className="flex flex-col xl:flex-row md:gap-6 xl:gap-14 gap-2 w-full">
-              <div className="flex flex-col xl:w-1/2 w-full relative">
-                <label
-                  htmlFor="phone"
-                  className="text-white xl:text-xl text-base font-bold"
-                >
-                  Phone
-                </label>
-                <img
-                  src={pokeball}
-                  alt=""
-                  className="md:size-12 xl:size-16 size-11 absolute md:top-[22px] xl:top-[26px] top-[24px]  md:-left-[17px] xl:-left-[23px] -left-[15px]"
-                />
-                <input
-                  type="tel"
-                  id="phone"
-                  {...register("phone")}
-                  name="entry.1956564773"
-                  className="bg-white xl:p-4 xl:pl-[50px] p-2 rounded-xl text-lg md:pl-[50px] pl-[30px]"
-                />
-                {errors.phone && (
-                  <span className="text-red-500 mx-2 px-2 text-sm">
-                    {errors.phone.message}
-                  </span>
-                )}
-              </div>
-              <div className="flex flex-col xl:w-1/2 w-full relative">
-                <label
-                  htmlFor="whatsapp"
-                  className="text-white xl:text-xl text-base font-bold"
-                >
-                  Whatsapp
-                </label>
-                <img
-                  src={pokeball}
-                  alt=""
-                  className="md:size-12 xl:size-16 size-11 absolute md:top-[22px] xl:top-[26px] top-[24px]  md:-left-[17px] xl:-left-[23px] -left-[15px]"
-                />
-                <input
-                  type="tel"
-                  id="whatsapp"
-                  {...register("whatsapp")}
-                  name="entry.922164047"
-                  className="bg-white xl:p-4 xl:pl-[50px] p-2 rounded-xl text-lg md:pl-[50px] pl-[30px]"
-                />
-                {errors.whatsapp && (
-                  <span className="text-red-500 mx-2 px-2 text-sm">
-                    {errors.whatsapp.message}
-                  </span>
-                )}
-              </div>
+            <PokeballInput
+              name="email"
+              label="Email (Personal)"
+              register={register}
+              errors={errors}
+              focusedField={focusedField}
+              handleFocus={handleFocus}
+              handleBlur={handleBlur}
+            />
+
+            <div className="flex flex-col xl:flex-row md:gap-6 xl:gap-8 gap-2 w-full">
+              <PokeballInput
+                name="phone"
+                label="Phone"
+                register={register}
+                errors={errors}
+                focusedField={focusedField}
+                handleFocus={handleFocus}
+                handleBlur={handleBlur}
+                onChange={(e) => setPhoneValue(e.target.value)}
+              />
+
+              <PokeballInput
+                name="whatsapp"
+                label="Whatsapp"
+                register={register}
+                errors={errors}
+                focusedField={focusedField}
+                handleFocus={handleFocus}
+                handleBlur={handleBlur}
+                value={watch("isSame") ? phoneValue : watch("whatsapp") || ""}
+                onChange={(e) => {
+                  if (!watch("isSame")) {
+                    setValue("whatsapp", e.target.value);
+                  }
+                }}
+              />
             </div>
 
             <div className="mt-2 z-20">
@@ -255,7 +208,7 @@ const Registration: React.FC = () => {
                 type="checkbox"
                 id="isSame"
                 {...register("isSame")}
-                name="entry.1669604705"
+                // name="entry.1669604705"
                 className="bg-white xl:p-4 p-2 rounded-xl text-lg pl-[50px]"
                 value="Yes"
               />
@@ -266,8 +219,8 @@ const Registration: React.FC = () => {
                 Phone Number is same as Whatsapp Number?
               </label>
               {errors.isSame && (
-                <span className="text-red-500 mx-2 px-2 text-sm">
-                  {errors.isSame.message}
+                <span className="text-yellow mx-2 px-2 text-sm font-bold">
+                  * {errors.isSame.message}
                 </span>
               )}
             </div>
